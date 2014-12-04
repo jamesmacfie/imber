b2ad7ab55899e9637682f92c08e302e04f1b8640
James Macfie <james@macfie.co.nz>
2014-12-05 07:46:30 +1300

    Splitting action buttons into their own templates

--- client/template.js
+++ client/template.js
@@ -1,7 +1,23 @@
+'use strict';
+
 Template.registerHelper('primaryIcon', {
     iconType: 'primary'
 });
 
 Template.registerHelper('secondaryIcon', {
     iconType: 'secondary'
+});
+
+Template.registerHelper('sprinklerActionButtons', function() {
+    switch(this.status) {
+        case 'active':
+            return 'sprinklerActionButtonsActive';
+        case 'inactive':
+            return 'sprinklerActionButtonsInactive';
+        case 'paused':
+            return 'sprinklerActionButtonsPaused';
+    }
+
+    console.warn('Unknown sprinker status of ' + this.status + ' for action buttons');
+    return 'sprinklerActionButtonsInactive';
 });
