5cd43bae0ccd72fcf9e7c0f2b8052497fd836986
James Macfie <james@macfie.co.nz>
2014-12-11 17:53:48 +1300

    Comment for templates and moving the file to a sane place

--- client/helpers/templates.js
+++ client/helpers/templates.js
@@ -1,13 +1,33 @@
 'use strict';
 
+/*
+ * Generic useful functions for templates
+ */
+
+/*
+ * Get's the name of the primary icon modifier class
+ *
+ * @returns {string} The modifier class
+ */
 Template.registerHelper('primaryIcon', {
     iconType: 'primary'
 });
 
+/*
+ * Get's the name of the secondary icon modifier class
+ *
+ * @returns {string} The modifier class
+ */
 Template.registerHelper('secondaryIcon', {
     iconType: 'secondary'
 });
 
+/*
+ * Returns the name of the template for showing the a sprinkler's action buttons
+ * given the scope of a sprinkler.
+ *
+ * @returns {string} The name of the button template
+ */
 Template.registerHelper('sprinklerActionButtons', function() {
     switch(this.status) {
         case 'active':
@@ -18,6 +38,7 @@ Template.registerHelper('sprinklerActionButtons', function() {
             return 'sprinklerActionButtonsPaused';
     }
 
+    // Just in case we hit something weird
     console.warn('Unknown sprinker status of ' + this.status + ' for action buttons');
     return 'sprinklerActionButtonsInactive';
 });
